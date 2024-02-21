c48347e6b0f15ef78c5b8a1fd93e40098e0c5966
Sochniynapas <66013035+Sochniynapas@users.noreply.github.com>
2024-02-21 23:35:27 +0700

    начал создавать карточку для конкретного объекта кабинета

---
+++ app/src/main/java/com/example/tsukeysmobile/Views/ReservationCard.kt
@@ -0,0 +1,15 @@
+package com.example.tsukeysmobile.Views
+
+
+import androidx.compose.runtime.Composable
+
+
+
+
+@Composable
+fun ReservationCard(
+    cab: String,
+    dateAndLes: String,
+) {
+
+}
