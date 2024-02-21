282ac3d83d1a298f0cc3616be90ea726f881d6e9
Sochniynapas <66013035+Sochniynapas@users.noreply.github.com>
2024-02-21 23:13:24 +0700

    начал создавать экран с кабинетами

---
+++ app/src/main/java/com/example/tsukeysmobile/Screens/Cab.kt
@@ -0,0 +1,10 @@
+package com.example.tsukeysmobile.Screens
+
+import androidx.compose.runtime.Composable
+
+import androidx.navigation.NavController
+
+@Composable
+fun CabScreen(navController: NavController, dateAndLes: String) {
+
+}
