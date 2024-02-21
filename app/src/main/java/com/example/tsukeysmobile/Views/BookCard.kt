360c8cc2996f698aa23b04da2b5892d2b0049fe4
Sochniynapas <66013035+Sochniynapas@users.noreply.github.com>
2024-02-21 21:43:50 +0700

    начал делать карточку для критериев фильтрации

---
+++ app/src/main/java/com/example/tsukeysmobile/Views/BookCard.kt
@@ -0,0 +1,14 @@
+package com.example.tsukeysmobile.Views
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun BookCard(navController: NavController) {
+
+}
