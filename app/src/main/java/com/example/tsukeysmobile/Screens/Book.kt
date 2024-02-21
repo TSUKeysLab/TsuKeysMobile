d153d9610990ccd4bbfcf95b05bb0608edc75bd6
Sochniynapas <66013035+Sochniynapas@users.noreply.github.com>
2024-02-21 21:29:37 +0700

    начал разработку экрана выбора параметров для фильтрации кабинетов

---
+++ app/src/main/java/com/example/tsukeysmobile/Screens/Book.kt
@@ -0,0 +1,17 @@
+package com.example.tsukeysmobile.Screens
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+
+import androidx.compose.runtime.Composable
+
+import androidx.navigation.NavController
+
+
+
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun BookScreen(navController: NavController) {
+
+}
