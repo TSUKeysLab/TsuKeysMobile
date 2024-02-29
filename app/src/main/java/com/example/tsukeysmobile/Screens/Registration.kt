633860470b370bb2ba14e101e4b8d33ef6b9d944
Sochniynapas <66013035+Sochniynapas@users.noreply.github.com>
2024-02-29 23:53:10 +0700

    сделал начальную верстку для регистрации

--- app/src/main/java/com/example/tsukeysmobile/Screens/Registration.kt
+++ app/src/main/java/com/example/tsukeysmobile/Screens/Registration.kt
@@ -2,17 +2,24 @@ package com.example.tsukeysmobile.Screens
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonColors
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.icons.materialIcon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -21,17 +28,20 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
 import com.example.tsukeysmobile.DefaultText
 import com.example.tsukeysmobile.Navigation.Screen
 import com.example.tsukeysmobile.R
 import com.example.tsukeysmobile.Views.BookCard
+import com.example.tsukeysmobile.Views.RegistrationCard
 import com.example.tsukeysmobile.ui.theme.backgroundCol1
 import com.example.tsukeysmobile.ui.theme.backgroundCol2
 
+@Preview
 @Composable
-fun RegistrationScreen(){
+fun RegistrationScreen() {
     Column(
         modifier = Modifier.fillMaxSize()
     )
@@ -44,9 +54,14 @@ fun RegistrationScreen(){
                 .zIndex(1f)
         )
         {
-            DefaultText(text = "регистрация", size = 70, modifier = Modifier.offset(x = 20.dp, y = 12.dp))
+            DefaultText(
+                text = "регистрация",
+                size = 55,
+                modifier = Modifier.offset(x = 20.dp, y = 12.dp)
+            )
         }
         Box(
+            contentAlignment = Alignment.Center,
             modifier = Modifier
                 .weight(1f)
                 .fillMaxWidth()
@@ -60,10 +75,9 @@ fun RegistrationScreen(){
                     )
                 )
                 .padding(horizontal = 20.dp),
-            contentAlignment = Alignment.Center,
         )
         {
-            //Здесь будет карточка регистрации
+            RegistrationCard()
         }
         Row(
             modifier = Modifier
@@ -74,7 +88,16 @@ fun RegistrationScreen(){
             verticalAlignment = Alignment.CenterVertically
         )
         {
-            // здесь будет кнопка
+            Button(
+                onClick = { /*TODO*/ },
+                Modifier
+                    .fillMaxWidth(0.4f)
+                    .background(Color.Cyan, shape = RoundedCornerShape(16.dp))
+                    .padding(horizontal = 8.dp),
+                colors = ButtonDefaults.buttonColors(backgroundColor = Color.White),
+            ) {
+                DefaultText(text = "Готово", size = 20, color = Color.Black, modifier = Modifier)
+            }
         }
     }
 }
--- app/src/main/java/com/example/tsukeysmobile/Views/RegistrationCard.kt
+++ app/src/main/java/com/example/tsukeysmobile/Views/RegistrationCard.kt
@@ -1,8 +1,167 @@
 package com.example.tsukeysmobile.Views
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.Create
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.tsukeysmobile.DefaultText
 
 @Composable
-fun RegistrationCard(){
+fun RegisterElementOutlined(fieldText: String, type: String) {
+    var text by remember { mutableStateOf(TextFieldValue("")) }
 
+    if (type == "Date") {
+        OutlinedTextField(
+            value = text,
+            onValueChange = {
+                text = it
+            },
+            modifier = Modifier
+                .fillMaxWidth(),
+            shape = RoundedCornerShape(8.dp),
+            textStyle = TextStyle(color = Color.Black),
+            label = { Text(fieldText) },
+            trailingIcon = { Icon(imageVector = Icons.Default.DateRange, contentDescription = null) },
+            keyboardOptions = KeyboardOptions(
+                capitalization = KeyboardCapitalization.None,
+                autoCorrect = true,
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Next
+            ),
+            visualTransformation = VisualTransformation.None,
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedContainerColor = Color.White,
+                unfocusedContainerColor = Color.White,
+                disabledContainerColor = Color.White,
+                focusedLabelColor = Color.White,
+                focusedBorderColor = Color.Black
+
+
+            )
+        )
+
+    } else if (type == "Gender") {
+        OutlinedTextField(
+            value = text,
+            onValueChange = {
+                text = it
+            },
+            modifier = Modifier
+                .fillMaxWidth(),
+            shape = RoundedCornerShape(8.dp),
+            textStyle = TextStyle(color = Color.Black),
+            label = { Text(fieldText) },
+            trailingIcon = { Icon(imageVector = Icons.Default.ArrowDropDown, contentDescription = null) },
+            keyboardOptions = KeyboardOptions(
+                capitalization = KeyboardCapitalization.None,
+                autoCorrect = true,
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Next
+            ),
+            visualTransformation = VisualTransformation.None,
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedContainerColor = Color.White,
+                unfocusedContainerColor = Color.White,
+                disabledContainerColor = Color.White,
+                focusedLabelColor = Color.White,
+                focusedBorderColor = Color.Black
+
+
+            )
+        )
+
+    } else {
+        OutlinedTextField(
+            value = text,
+            onValueChange = {
+                text = it
+            },
+            modifier = Modifier
+                .fillMaxWidth(),
+            shape = RoundedCornerShape(8.dp),
+            textStyle = TextStyle(color = Color.Black),
+            label = { Text(fieldText) },
+            keyboardOptions = KeyboardOptions(
+                capitalization = KeyboardCapitalization.None,
+                autoCorrect = true,
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Next
+            ),
+            visualTransformation = VisualTransformation.None,
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedContainerColor = Color.White,
+                unfocusedContainerColor = Color.White,
+                disabledContainerColor = Color.White,
+                focusedLabelColor = Color.White,
+                focusedBorderColor = Color.Black
+
+
+            )
+        )
+    }
+}
+
+@Composable
+fun RegisterElement(topLabel: String, text: String, type: String) {
+    Column(modifier = Modifier.fillMaxWidth(0.9f), horizontalAlignment = Alignment.Start) {
+        DefaultText(text = topLabel, size = 20, modifier = Modifier.offset(y = 12.dp))
+        Spacer(modifier = Modifier.height(12.dp))
+        RegisterElementOutlined(text, type)
+    }
+}
+
+@Preview
+@Composable
+fun RegistrationCard() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.Black, RoundedCornerShape(16.dp)),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    )
+    {
+        RegisterElement("имя", "admin", "")
+        RegisterElement("фамилия", "admin", "")
+        RegisterElement("дата рождения", "26.02.2003", "Date")
+        RegisterElement("пол", "мужской", "Gender")
+        RegisterElement("email", "admin@example.com", "")
+        RegisterElement("пароль", "********", "")
+        RegisterElement("повторите пароль", "********", "")
+
+    }
 }
