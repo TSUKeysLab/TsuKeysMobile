fb7670604c28cd1f25fa327803963f1a1da9690b
Sochniynapas <66013035+Sochniynapas@users.noreply.github.com>
2024-03-01 14:16:42 +0700

    сделал выпадающий список для гендера и настроил его

--- app/src/main/java/com/example/tsukeysmobile/Views/RegistrationCard.kt
+++ app/src/main/java/com/example/tsukeysmobile/Views/RegistrationCard.kt
@@ -1,6 +1,7 @@
 package com.example.tsukeysmobile.Views
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -9,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
@@ -16,7 +18,11 @@ import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.Create
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -30,6 +36,9 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardCapitalization
@@ -38,12 +47,22 @@ import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.toSize
 import com.example.tsukeysmobile.DefaultText
 
 @Composable
 fun RegisterElementOutlined(fieldText: String, type: String) {
-    var text by remember { mutableStateOf(TextFieldValue("")) }
 
+    val listOfGenders = listOf("мужчина", "женщина")
+    var text by remember { mutableStateOf(TextFieldValue("")) }
+    var expanded by remember { mutableStateOf(false) }
+    var selectedItem by remember { mutableStateOf("") }
+    var textFieldSize by remember { mutableStateOf(Size.Zero) }
+    val icon = if (expanded) {
+        Icons.Filled.KeyboardArrowUp
+    } else {
+        Icons.Filled.KeyboardArrowDown
+    }
     if (type == "Date") {
         OutlinedTextField(
             value = text,
@@ -55,7 +74,12 @@ fun RegisterElementOutlined(fieldText: String, type: String) {
             shape = RoundedCornerShape(8.dp),
             textStyle = TextStyle(color = Color.Black),
             label = { Text(fieldText) },
-            trailingIcon = { Icon(imageVector = Icons.Default.DateRange, contentDescription = null) },
+            trailingIcon = {
+                Icon(
+                    imageVector = Icons.Default.DateRange,
+                    contentDescription = null
+                )
+            },
             keyboardOptions = KeyboardOptions(
                 capitalization = KeyboardCapitalization.None,
                 autoCorrect = true,
@@ -68,47 +92,71 @@ fun RegisterElementOutlined(fieldText: String, type: String) {
                 unfocusedContainerColor = Color.White,
                 disabledContainerColor = Color.White,
                 focusedLabelColor = Color.White,
-                focusedBorderColor = Color.Black
-
-
-            )
+                focusedBorderColor = Color.Black,
+            ),
+            maxLines = 1
         )
 
     } else if (type == "Gender") {
+
         OutlinedTextField(
-            value = text,
-            onValueChange = {
-                text = it
-            },
+            value = selectedItem,
+            onValueChange = { selectedItem = it },
             modifier = Modifier
-                .fillMaxWidth(),
-            shape = RoundedCornerShape(8.dp),
-            textStyle = TextStyle(color = Color.Black),
-            label = { Text(fieldText) },
-            trailingIcon = { Icon(imageVector = Icons.Default.ArrowDropDown, contentDescription = null) },
-            keyboardOptions = KeyboardOptions(
-                capitalization = KeyboardCapitalization.None,
-                autoCorrect = true,
-                keyboardType = KeyboardType.Text,
-                imeAction = ImeAction.Next
-            ),
-            visualTransformation = VisualTransformation.None,
+                .fillMaxWidth()
+                .onGloballyPositioned { coordinates ->
+                    textFieldSize = coordinates.size.toSize()
+                },
+            label = {
+                Text(
+                    text = "Выберите пол", color = if (selectedItem != "") {
+                        if (expanded == true) {
+                            Color.White
+                        } else {
+                            Color.DarkGray
+                        }
+                    } else {
+                        Color.Black
+                    }
+                )
+            },
+            trailingIcon = {
+                Icon(icon, "", Modifier.clickable() { expanded = !expanded })
+            },
+            enabled = false,
             colors = OutlinedTextFieldDefaults.colors(
                 focusedContainerColor = Color.White,
                 unfocusedContainerColor = Color.White,
                 disabledContainerColor = Color.White,
-                focusedLabelColor = Color.White,
-                focusedBorderColor = Color.Black
 
-
-            )
+                ),
         )
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+            modifier = Modifier
+                .width(with(LocalDensity.current) { textFieldSize.width.toDp() })
+        )
+        {
+            listOfGenders.forEach { label ->
+                DropdownMenuItem(
+                    text = {
+                        Text(text = label, color = Color.Black)
+                    }, onClick = {
+                        selectedItem = label
+                        expanded = false
+                    })
+            }
+        }
 
     } else {
+        val maxChar = 20
         OutlinedTextField(
             value = text,
             onValueChange = {
-                text = it
+                if (it.text.length <= maxChar) {
+                    text = it
+                }
             },
             modifier = Modifier
                 .fillMaxWidth(),
@@ -127,13 +175,15 @@ fun RegisterElementOutlined(fieldText: String, type: String) {
                 unfocusedContainerColor = Color.White,
                 disabledContainerColor = Color.White,
                 focusedLabelColor = Color.White,
-                focusedBorderColor = Color.Black
+                focusedBorderColor = Color.Black,
 
 
-            )
+                ),
+            maxLines = 1,
         )
     }
 }
+
 
 @Composable
 fun RegisterElement(topLabel: String, text: String, type: String) {
